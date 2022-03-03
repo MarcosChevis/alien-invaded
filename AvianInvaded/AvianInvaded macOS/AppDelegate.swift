15af2ac2b73999577f90b38e8eec5f81a3b01050
Marcos Chevis <marcoschevism@gmail.com>
2022-03-03 16:26:28 -0300

    initial xCode setup.

---
+++ AvianInvaded/AvianInvaded macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  AvianInvaded macOS
+//
+//  Created by Marcos Chevis on 03/03/22.
+//
+
+import AppKit
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
