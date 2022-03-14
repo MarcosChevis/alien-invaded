c45f0b262bac85dd371876439012cf1ca08c1cb2
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2022-03-14 14:17:28 -0300

    started roomBuilder

---
+++ AvianInvaded/AvianInvaded Shared/Data/Room/Room.swift
@@ -0,0 +1,8 @@
+//
+//  Room.swift
+//  AvianInvaded iOS
+//
+//  Created by Gabriel Ferreira de Carvalho on 14/03/22.
+//
+
+import Foundation
---
+++ AvianInvaded/AvianInvaded Shared/Logic/Room/RoomBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  RoomBuilder.swift
+//  AvianInvaded iOS
+//
+//  Created by Gabriel Ferreira de Carvalho on 14/03/22.
+//
+
+import Foundation
